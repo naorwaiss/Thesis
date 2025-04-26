ea753225d97c9e3006e486202db9f8f87c003407
Naor Weiss <naorw@post.bgu.ac.il>
2025-04-26 10:58:37 +0300

    fix the ackerman control

--- install/ground_bot/share/ground_bot/launch/launch/hamma_bot.launch.py
+++ install/ground_bot/share/ground_bot/launch/launch/hamma_bot.launch.py
@@ -72,6 +72,7 @@ def generate_launch_description():
             # '/model/x8_bot/odometry@nav_msgs/msg/Odometry[gz.msgs.Odometry]'
             # "/model/x8_bot/depth_camera@sensor_msgs/msg/Image@ignition.msgs.Image",
             # '/model/x8_bot/depth_camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo]',
+            "/scan@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan",
             "/imu@sensor_msgs/msg/Imu[ignition.msgs.IMU",
             '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock]',
 
